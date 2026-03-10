beb18cef7f2cd619989fa5213da1f74e4758c4c1
Everton Carlos <Evertonlensson@hotmail.com>
2026-03-10 18:51:44 -0300

    Adicionando exercícios de variáveis em Javascript

---
+++ dia-01-variaveis/script.js
@@ -0,0 +1,36 @@
+// Dia 01 = Exercícios variáveis
+
+// Exercício 1 - Soma de número, crie duas variáveis e mostre a soma deles.
+let numUm = 10,
+  numDois = 5;
+let resultadoSoma = numUm + numDois;
+console.log(resultadoSoma);
+
+// Exercício 2 - Média de aluno, crie três notas e calcule a média.
+let notaUm = 7,
+  notaDois = 7,
+  notaTres = 7;
+let mediaNota = (notaUm + notaDois + notaTres) / 3;
+console.log(`A sua media é ${mediaNota}`);
+
+// Exercício 3 - Converter horas em minutos, crie uma variável com horas e converta para minutos.
+let horas = 2;
+let minutos = horas * 60;
+console.log(minutos, `minutos`);
+
+// Exercício 4 - Dobro de um número, crie uma variável número e mostre o dobro.
+let numero = 6;
+let resultadoDobro = numero * 2;
+console.log(`O dobro de ${numero} é ${resultadoDobro}`);
+
+// Exercício 5 - Preço com desconto
+let produto = 200,
+  desconto = 0.1;
+let precoFinal = produto - produto * desconto;
+console.log(precoFinal);
+
+// Exercício 6 - Calcular a idade de uma pessoa
+let anoAtual = 2026,
+  anoNascimento = 1998;
+let idade = anoAtual - anoNascimento;
+console.log(idade);
